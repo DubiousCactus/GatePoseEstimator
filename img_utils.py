4797b43004fe99847867059474a0eb8ea81b4e79
Theo Morales <theo.morales.fr@gmail.com>
2019-08-31 20:05:19 +0200

    Center the crop

--- img_utils.py
+++ img_utils.py
@@ -20,11 +20,14 @@ Several image manipulation utility functions
     zero-padding to the rest of the image. The returned image in fact has the
     same dimensions as the given image
 '''
-def crop_and_pad(img, corner_min, corner_max):
+def crop_and_pad(img, corner_min, corner_max, centered=True):
     cropped = np.zeros(img.shape, dtype=img.dtype)
-    cropped[
-        corner_min[1]:corner_max[1],
-        corner_min[0]:corner_max[0],:
-    ] = img[corner_min[1]:corner_max[1], corner_min[0]:corner_max[0],:]
+    crop = img[corner_min[1]:corner_max[1], corner_min[0]:corner_max[0],:]
+    if centered:
+        startW = (img.shape[1] - crop.shape[1]) / 2
+        startH = (img.shape[0] - crop.shape[0]) / 2
+        cropped[startH:startH+crop.shape[0], startW:startW+crop.shape[1]] = crop
+    else:
+        cropped[corner_min[1]:corner_max[1], corner_min[0]:corner_max[0],:] = crop
     assert cropped.shape == img.shape, "Cropped image has been resized!"
     return cropped
